5493947939698bee6791a5890c6055aa7bce65e3
Amritesh Nandan <amriteshwork@gmail.com>
2023-03-27 16:29:24 +0530

    first commit and added raw data

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='this is a end to end mlops project using dvc',
+    author='Amritesh Nandan',
+    license='MIT',
+)
